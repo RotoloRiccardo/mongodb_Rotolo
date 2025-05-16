99420943e6a3f2a5748269f80292737f38152c94
Riccardo Rotolo <riccardo.rotolo@isisbem.it>
2025-05-16 08:26:27 +0200

    Aggiunto il nome della pagina

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const dev_db_url =
-  "mongodb+srv://<rotoloriccardo>:<mauromarchesan>@cluster0.1c15lej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://riccardorotolo:mauromarchesan@cluster0.1c15lej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
